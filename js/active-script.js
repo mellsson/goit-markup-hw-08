9d6dcdb8d2ea8e325836cec002aa818cf54cdbf3
mellsson <68289519+mellsson@users.noreply.github.com>
2020-09-13 18:53:09 +0300

    Suumary

---
+++ js/active-script.js
@@ -0,0 +1,10 @@
+ $(document).ready(function () {
+$('.menu-list li a').each(function () {
+    var location = window.location.href;
+        var link = this.href;
+        if (location == link) {
+            $(this).addClass('active');
+        }
+    });
+});
+  
